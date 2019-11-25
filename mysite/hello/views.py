8a18dff46969820df0ebf407d5a99cf1029f38b3
codrelphi <chancerel.codjovi@gmail.com>
2019-11-25 20:33:21 +0100

    create some views

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def message(request):
+    message= """
+        <h1>Message</h1>
+        <p>Un message est un ensemble de signes. Il implique donc un codage par l'émetteur, et un décodage par le récepteur
+        (d'où la nécessité d'un code commun). La théorie de l'information fut mise au point pour déterminer mathématiquement
+        le taux d’information transmis dans la communication d’un message par un canal de communication, notamment en présence
+        de parasites appelés bruits. Il fut repris par Roman Jakobson pour étayer la théorie linguistique.</p>
+    """
+    return HttpResponse(message)
