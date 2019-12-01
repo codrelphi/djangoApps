3879af119f76cf7f45227eb58f117a31761c9ecf
codrelphi <chancerel.codjovi@gmail.com>
2019-12-01 20:47:09 +0100

    add some views

--- todolist/taskslist/views.py
+++ todolist/taskslist/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Task
+
+def index(request):
+    context = {
+        "tasks": Task.objects.all()
+    }
+    return render(request, "taskslist/index.html", context)
+
+def addTask(request):
+    try:
+        name = request.POST['task-name']
+    except:
+        return render(request, "taskslist/error.html", {"message": "No Task."})
+
+    Task(name=name).save()
+    return HttpResponseRedirect(reverse("index"))
