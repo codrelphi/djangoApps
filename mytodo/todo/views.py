7f46b156e924485ac4a7a2e9e11dedd4464079e6
codrelphi <chancerel.codjovi@gmail.com>
2019-12-04 13:51:12 +0100

    fix the view "add" to display some messages

--- mytodo/todo/views.py
+++ mytodo/todo/views.py
@@ -10,12 +10,26 @@ def index(request):
     }
     return render(request, "todo/index.html", context)
 
+# TODO: pouvoir faire la redirection tout en affichant le contenu de la
+# variable "message"
 def add(request):
     todo = request.POST['todo-input']
     if len(todo) > 0:
-        Todo(name=todo).save()
+        try:
+            if Todo.objects.get(name=todo):
+                message = "This task has already existed"
+        except Todo.DoesNotExist:
+            message=""
+            Todo(name=todo).save()
+    else:
+        message = "Please, add a task!"
 
-    return HttpResponseRedirect(reverse("index"))
+    context = {
+        "todo_list": Todo.objects.all(),
+        "message": message
+    }
+    return render(request, "todo/index.html", context)
+    #return HttpResponseRedirect(reverse("index"), context)
 
 def delete(request, todo_id):
     Todo.objects.get(pk=todo_id).delete()
