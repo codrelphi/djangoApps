4bd94a15a66fe8a5ed5b2167956025d8214e9d96
codrelphi <chancerel.codjovi@gmail.com>
2019-12-04 01:34:00 +0100

    update

--- mytodo/todo/views.py
+++ mytodo/todo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Todo
+
+def index(request):
+    context = {
+        "todo_list": Todo.objects.all()
+    }
+    return render(request, "todo/index.html", context)
+
+def add(request):
+    todo = request.POST['todo-input']
+    if len(todo) > 0:
+        Todo(name=todo).save()
+
+    return HttpResponseRedirect(reverse("index"))
+
+def delete(request, todo_id):
+    Todo.objects.get(pk=todo_id).delete()
+    return HttpResponseRedirect(reverse("index"))
